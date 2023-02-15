5517ec9f60aba17e16ed3b5805984ba5686f6271
ankeshaj37 <ankeshaj37@gmail.com>
2023-02-15 07:00:11 +0530

    aa

--- src/Popul.js
+++ src/Popul.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { db } from "./firebase";
- 
+
 const Popul = () => {
 
   const [popu, setPopu] = useState([])
 
   useEffect(() => {
-      db.collection('populernews').onSnapshot(tap => (
-        setPopu(tap.docs.map((e) => ({uid:e.id,data:e.data()})))
-      ))
+    db.collection('populernews').onSnapshot(tap => (
+      setPopu(tap.docs.map((e) => ({ uid: e.id, data: e.data() })))
+    ))
   }, [])
   return (
     <>
@@ -17,44 +17,45 @@ const Popul = () => {
         <div class="  container HomepageBlogPosts">
           <div
             class="title-main text-center mx-auto mb-4">
-           <div className="brekname">
-           <h4 className="alltitle">Populer News</h4>
-           </div>
-            
-          </div>
-          <div class=" popdiv row justify-content-center">
-          {popu.map((e)=>(
-                <>
-            <div class="col-lg-4 col-sm-6 mt-sm-5 pt-lg-2">
-             
-                      <div class="  grids5-info">
-             
-                  <div data-aos="fade-up"
-     data-aos-duration="3000" class=" cc post-thumbnail">
-                    <img 
-                      width="300px"
-                      height="300px"
-                      src={e.data.image}
-                      class="attachment-post-thumbnail size-post-thumbnail wp-post-image"
-                      alt=""
-                    />
-                      <div class="blog-info">
-                  <p>{e.data.title}</p>
-                  <Link to={`/${e.uid}`} class="btn btn-style mt-4" href={e.data.video}>
-                    Read More
-                  </Link>
-                </div>
-                  </div>
-             
-              
-              </div>
-              
+            <div className="brekname">
+              <h4 className="alltitle">Populer News</h4>
             </div>
 
-            </>
-              ))}
+          </div>
+          <div class=" popdiv row justify-content-center">
+            {popu.map((e) => (
+              <>
+                <div class="col-lg-4 col-sm-6 mt-sm-5 pt-lg-2">
 
-          
+                  <div class="  grids5-info">
+
+                    <div data-aos="fade-up"
+                      data-aos-duration="3000" class="  post-thumbnail">
+                     
+                      <img className="cc"
+                       
+                        src={e.data.image}
+                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image"
+                        alt=""
+                      />
+                   
+                      <div class="blog-info">
+                        <p>{e.data.title}</p>
+                        <Link to={`/:id/${e.uid}`} class="btn btn-style mt-4" href={e.data.video}>
+                          Read More
+                        </Link>
+                      </div>
+                    </div>
+
+
+                  </div>
+
+                </div>
+
+              </>
+            ))}
+
+
           </div>
         </div>
       </div>
